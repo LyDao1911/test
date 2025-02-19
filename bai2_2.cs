1f9ca71ede78e3fb545a724769490640e61343c6
LyDao1911 <lydao191120004@gmail.com>
2025-02-19 13:03:45 +0700

    add code

---
+++ bai2_2.cs
@@ -0,0 +1,28 @@
+﻿namespace Bai1
+{
+    class bai2_2
+    {
+        static void Main(string[] args)
+        {
+            float a, b;
+            Console.Write("Nhap vao so a :");
+            a = float.Parse(Console.ReadLine());
+            Console.Write("Nhap vao so b:");
+            b = Convert.ToSingle(Console.ReadLine());
+            //Neu a==0 thi (1)<=> b==0
+            if (a == 0)
+            {
+                //Neu b==0 thi pt vo so nghiem
+                if (b == 0)
+                {
+                    Console.WriteLine("PT vo so nghiem!");
+                    //nguoc lai neu b!=0 thi pt vo nghiem
+                }
+                else Console.WriteLine("PT vo nghiem");
+            }
+            else Console.WriteLine($"PT co 1 nghiem : x= {-b / a}");
+            //Neu a!=0 thi pt co mot nghiem x=-b/a
+
+        }
+    }
+}
