e71e3d73a90e153d32bd5112a614a521721528aa
LyDao1911 <lydao191120004@gmail.com>
2025-02-19 12:59:01 +0700

    day la test

---
+++ Bai2_1.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello, World!");
+string maSV, hoTen, ngaySinh;
+Console.Write("Nhap ma sinh vien: ");
+maSV = Console.ReadLine();
+Console.Write("Nhap Ho va Ten: ");
+hoTen = Console.ReadLine();
+Console.Write("Nhap ngay thang nam sinh: ");
+ngaySinh = Console.ReadLine();
+
+Console.WriteLine("Ma sinh vien: " + maSV);
+Console.WriteLine("Ho va Ten: " + hoTen);
+Console.WriteLine("Ngay thang nam sinh: " + ngaySinh);
